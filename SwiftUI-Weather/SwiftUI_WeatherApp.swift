ce7db7fd169de6f06d4f8705609098dd82a76311
PN <priteshrna@gmail.com>
2021-11-18 19:34:16 -0500

    initial commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Pritesh Nadiadhara on 11/18/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
