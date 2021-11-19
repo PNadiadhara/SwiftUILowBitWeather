d5531a7cb61f348dd939039a903adf3245886a4b
PN <priteshrna@gmail.com>
2021-11-18 19:50:41 -0500

    vstack adjustments

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -14,13 +14,24 @@ struct ContentView: View {
                            startPoint: .topLeading,
                            endPoint: .bottomTrailing).edgesIgnoringSafeArea(.all)
             
-            VStack {
+            VStack{
                 Text("Location Of You").font(.system(size: 32,weight: .medium, design: .default))
                     .foregroundColor(.white)
                     .padding()
+                
+                VStack(spacing: 10)  {
+                    Image(systemName: "cloud.sun.fill").renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    Text("76")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                }
+                
                 Spacer()
-                    
-                    
+                
             }
         }
     }
